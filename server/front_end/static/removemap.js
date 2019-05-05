0ba156e257adf2488b7113eaec8e6da336fb1316
Nathan G <nathan.green@ymail.com>
2019-05-04 20:22:18 -0400

    Changed quite a hefty volume of code

---
+++ server/front_end/static/removemap.js
@@ -0,0 +1,3 @@
+function removePackage(){
+document.getElementById("package").remove();
+}
--- server/front_end/static/tts.js
+++ server/front_end/static/tts.js
@@ -1,4 +1,4 @@
 function tts() {
-    var msg = new SpeechSynthesisUtterance('Welcome to LiveTracker.  Press tab then enter your tracking numbers separated by spaces and hit Enter to continue.');
+    var msg = new SpeechSynthesisUtterance('Welcome to LliveTracker.  Press tab then enter your tracking numbers separated by spaces and hit Enter to continue.');
     window.speechSynthesis.speak(msg);
 }
